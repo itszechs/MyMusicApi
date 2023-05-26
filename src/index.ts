b4064dfc21269bb84ff42d79b2046584c4501f5d
zechs <vijay.vky2002@gmail.com>
2023-05-26 23:43:46 +0530

    updated `index.ts` - connect to database - use `.env` if available

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,37 @@
+import dotenv from 'dotenv'
 import express, {NextFunction, Request, Response} from "express";
+import {connectToDatabase} from "./services/database";
 
-const app = express();
-app.listen(5500, () => {
-    console.log(`Server running at http://localhost:5500`);
-});
+// Load environment variables from the .env file, where the ATLAS_URI is configured
+dotenv.config();
 
-// Health check route
-app.get("/", (_req: Request, res: Response) => {
-    res.send({
-        message: "Api is active!",
-    });
-});
+const {ATLAS_URI} = process.env;
 
-// Invalid route message
-app.use((_req: Request, res: Response, _next: NextFunction) => {
-    res.status(404).send({
-        error: "Not Found",
-    });
-});
+if (!ATLAS_URI) {
+    console.error("No ATLAS_URI environment variable has been defined in config.env");
+    process.exit(1);
+}
 
+connectToDatabase(ATLAS_URI)
+    .then(() => {
+        const app = express();
+
+        app.listen(5500, () => {
+            console.log(`Server running at http://localhost:5500`);
+        });
+
+        // Health check route
+        app.get("/", (_req: Request, res: Response) => {
+            res.send({
+                message: "Api is active!",
+            });
+        });
+
+        // Invalid route message
+        app.use((_req: Request, res: Response, _next: NextFunction) => {
+            res.status(404).send({
+                error: "Not Found",
+            });
+        });
+
+    }).catch(error => console.error(error));
